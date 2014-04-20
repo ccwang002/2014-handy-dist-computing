e9fb3d3bf6a8dc4a03e4a560420170ed30d1c1ac
Liang Bo Wang <ccwang002@gmail.com>
2014-04-21 03:37:33 +0800

    try another alg

--- demo_code/builtin-cpuheavy/better_validate_prime.py
+++ demo_code/builtin-cpuheavy/better_validate_prime.py
@@ -1,17 +1,25 @@
 from concurrent.futures import ProcessPoolExecutor
 from validate_prime import PRIMES
+import math
 
-def better_is_prime(number):
-    if number <= 1 or number % 2 == 0:
+
+def better_is_prime(n):
+    if n == 2 or n == 3:
+        return True
+    elif n < 2 or n % 2 == 0:
         return False
-    check = 3
-    maxneeded = number
-    while check < maxneeded + 1:
-        maxneeded = number / check
-        if number % check == 0:
+    elif n < 9:
+        return True
+    elif n % 3 == 0:
+        return False
+    r = int(math.sqrt(n + 1))
+    f = 5
+    while f <= r:
+        if n % f == 0 or n % (f + 2) == 0:
             return False
-        check += 2
-    return True
+        else:
+            f += 6
+            return True
 
 def better_process():
     with ProcessPoolExecutor() as executor:

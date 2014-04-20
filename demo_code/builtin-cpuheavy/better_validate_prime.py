2a0cd3bc3d53082c25e3799323b98fc406f9cc85
Liang Bo Wang <ccwang002@gmail.com>
2014-04-21 03:56:55 +0800

    use numpy

--- demo_code/builtin-cpuheavy/better_validate_prime.py
+++ demo_code/builtin-cpuheavy/better_validate_prime.py
@@ -1,26 +1,19 @@
 from concurrent.futures import ProcessPoolExecutor
 from validate_prime import PRIMES
-import math
-
+import numpy as np
 
 def better_is_prime(n):
-    if n == 2 or n == 3:
-        return True
-    elif n < 2 or n % 2 == 0:
+    if np.mod(n, 2) == 0:
         return False
-    elif n < 9:
-        return True
-    elif n % 3 == 0:
-        return False
-    r = int(math.sqrt(n + 1))
-    f = 5
-    while f <= r:
-        if n % f == 0 or n % (f + 2) == 0:
+
+    sqrt_n = np.int(np.sqrt(n)) + 1
+
+    for i in range(3, sqrt_n, 2):
+        if np.mod(n, 1) == 0:
             return False
-        else:
-            f += 6
 
     return True
+
 
 def better_process():
     with ProcessPoolExecutor() as executor:
